5b1b5c7e3fe3c2c71455f1643297ea38e4a8a326
Stefan Nedelcu <stefan.n@gmx.co.uk>
2021-05-15 17:02:04 +0100

    commit on 'main'

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 
-// data files
-const decks = require("./data/decks.json")
-
 const app = express();
 
 // allow api to be used 
 app.use(cors());
 
 app.get('/', (req,res) => {
-    res.send(decks)
+    res.send("home page")
 })
 
 // middleware
 const set1Routes = require("./routes/set1");
 app.use('/set1', set1Routes);
 
-app.listen(3001, () => console.log("listening at port 30001"));
+app.listen(3001, () => console.log("listening at port 3001"));
--- pages/topic1.js
+++ pages/topic1.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Link from "next/Link"
 import Flashcard from "./Flashcard";
+const axios = require('axios')
 
 export default function topic1() {
+    const [data, setData] = useState("");
+    useEffect(() => {
+        axios.get('http://localhost:3001/set1')
+        .then(function (response) {
+            // get data
+            setData(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }, [])
     return (
         <div style={{display: 'flex',height:'90vh', flexDirection:'column', alignItems:'center', justifyContent:'space-around'}}>
             <div className="title-part">
