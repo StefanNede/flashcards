1375233a65d4440156ef3054e300ed01f676acd5
Stefan Nedelcu <74429608+StefanNede@users.noreply.github.com>
2021-05-27 23:50:17 +0100

    Add files via upload

--- pages/Flashcard.jsx
+++ pages/Flashcard.jsx
@@ -33,7 +33,8 @@ export default function Flashcard( {frontImage, backImage ,lengthOfData, front, 
             <button className="show-progress-bar" onClick = {() => setShowProgressBar(!showProgressBar)}>{showProgressBar ? "hide " : "show "} progress bar</button>
             <div className={onBack ? "onBack flashcard" : "flashcard"}>
                 <p className={onBack ? "pOnBack" : ""}>{onBack ? back.map((backStuff) => <div>{backStuff}</div>) : front}</p>
-                {onBack ? (backImage == "" ? <></>:<img className={onBack ? "pOnBack" : ""} src={backImage} alt="image" height="80%"/>) : (frontImage === "" ? <></> : <img className={onBack ? "pOnBack" : ""} src={frontImage} alt="image" height="80%"/>)}
+                <br/>
+                {onBack ? (backImage == "" ? <></>:<img className={onBack ? "pOnBack" : ""} src={backImage} alt="image" width="80%"/>) : (frontImage === "" ? <></> : <img className={onBack ? "pOnBack" : ""} src={frontImage} alt="image" width="80%" />)}
             </div>
             <div className="options">
                 <button className="previous option-button" onClick={moveBack}>previous</button>
